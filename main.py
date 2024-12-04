c4a3be9dac6cd68b672ceffafd9f9edbadcd857b
Priyanshu0931 <priyanshujha2002@gmail.com>
2024-12-04 23:04:29 +0530

    ee check

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # necessary imports
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import pysqlite3
+import sqlite3
 import streamlit as st
 import os
 import openai
